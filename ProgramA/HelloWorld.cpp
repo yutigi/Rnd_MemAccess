921df15b9c74f0280aafb574f7328ee329f4a5a9
top <js.sookudom@gmail.com>
2024-10-27 22:10:36 +0700

    solution #1 to read the memory address of the other program.

---
+++ ProgramA/HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+static int static_value = 99;
+
+
+
+int main() {
+    //int static_value = 99;
+    cout << static_value << endl;
+    cout << &static_value << endl;
+
+    HWND dummyHWND = ::CreateWindowA("STATIC","dummy",
+        WS_VISIBLE,0,0,100,100,NULL,NULL,NULL,NULL);
+    ::SetWindowTextA(dummyHWND,"Dummy Window!");
+    ShowWindow(dummyHWND,SW_SHOW);
+
+    system("pause");
+    return 0;
+}
